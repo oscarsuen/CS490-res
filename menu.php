d18d8260048b90bfa9c93a44632246edcae7a40b
lefriedberg <lefriedberg@mail.ru>
2015-10-27 10:58:59 -0400

    updated menu.php

--- menu.php
+++ menu.php
@@ -3,57 +3,40 @@
 		
 		$errorMessage = "";
 
-		$varTitle = $_POST['title'];
-		$varAuthor = $_POST['author'];
-		$varISBN = $_POST['ISBN'];
-		$varCall_no = $_POST['call_no'];
+		$name = $_POST['Item Name'];
+		$price = $_POST['Price'];
+
         
         //make sure all fields are complete
-		if(empty($varTitle)) {
-			$errorMessage .= "Title";
+		if(empty($name)) {
+			$errorMessage .= "name";
 			echo "<div class='alert alert-danger' role='alert'>";
 			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
 			echo "<span class='sr-only'>Error:</span>";
-  			echo " You forgot to enter the title!";
+  			echo " You forgot to enter item name!";
   			echo "</div>";
 
 		}
-		if(empty($varAuthor)) {
-			$errorMessage .= "Author";
-			echo "<div class='alert alert-danger' role='alert'>";
+        if(0 == preg_match("/^\d\d?\.\d?\d?$/", $price)){
+            $errorMessage .= "price";
+            echo "<div class='alert alert-danger' role='alert'>";
 			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
 			echo "<span class='sr-only'>Error:</span>";
-  			echo " You forgot to enter the title!";
+  			echo " Invalid Price!";
   			echo "</div>";
-		}
-		if(empty($varISBN)) {
-			$errorMessage .= "ISBN";
-			echo "<div class='alert alert-danger' role='alert'>";
-			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
-			echo "<span class='sr-only'>Error:</span>";
-  			echo " You forgot to enter the title!";
-  			echo "</div>";
-		}
-		if(empty($varCall_no)) {
-			$errorMessage .= "Call_No";
-			echo "<div class='alert alert-danger' role='alert'>";
-			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
-			echo "<span class='sr-only'>Error:</span>";
-  			echo " You forgot to enter the title!";
-  			echo "</div>";
-		}
+        }
+
+
 		
 		if(empty($errorMessage)) {
 			$db = mysql_connect("localhost","root",""); //connect to database
 			
 			if(!$db) die("Error connecting to MySQL database.");
-			mysql_select_db("library" ,$db);
+			mysql_select_db("delivery" ,$db);
 			
-			$sql = "INSERT INTO book (title, author, ISBN, call_no) VALUES (".
-			PrepSQL($varTitle) . ", " .
-			PrepSQL($varAuthor) . ", " .
-			PrepSQL($varISBN) . ", " .
-			PrepSQL($varCall_no) . ")";
+			$sql = "INSERT INTO food (name, price) VALUES (".
+			PrepSQL($name) . ", " .
+			PrepSQL($price) . ")";
 			mysql_query($sql);
 
 			$rollno = mysql_query("SELECT ID FROM book ORDER BY id DESC LIMIT 1");
@@ -74,7 +57,7 @@
 
 			exit();
 		} else {
-			echo "<a href='addbook.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Try Again</a>";
+			echo "<a href='menu.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Try Again</a>";
 			}
 
 	}
