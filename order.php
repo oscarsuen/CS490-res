666f211fbbaf5b023db96dd9b8e47fe92c228d9b
lefriedberg <lefriedberg@mail.ru>
2015-10-29 09:14:04 -0400

    updating order.php

--- order.php
+++ order.php
@@ -1,7 +1,42 @@
-<?php
+<HTML>
+    <head><title>Add Menu Item</title></head>
+    <body>
+        Add Menu Item
+        <form action = "checkout.php" method="post">
+           <table border = 1>
+            
+            Item name <input type = "text" name ="Name"><br>
+            Price<input type = "text" name="Price"><br>
 
-    $db = mysql_connect("localhost","root","");
+            <?php
 
-            if(!$db) die("Error connecting to MySQL database.");
-            mysql_select_db("delivery" ,$db);
-?>
+                $db = mysql_connect("localhost","root","");
+
+                        if(!$db) die("Error connecting to MySQL database.");
+                        mysql_select_db("delivery" ,$db);
+               
+                $result = mysql_query("SELECT * FROM food");
+
+                echo "<h3>Books</h3><br/ ><table class='table table-striped'><tr><td>ID</td><td>Title</td><td>Author</td><td>ISBN</td><td>Call No</td><td>Shelf Status</td></tr>";
+                while($row= mysql_fetch_array($bookresult))
+                      echo("<tr><td>".
+                            $row['ID']."</td><td>".
+                            $row['title']."</td><td>".
+                            $row['author']."</td><td>".
+                            $row['ISBN']."</td><td>".
+                            $row['call_no']."</td><td>".
+                            $row['shelf_status']."</td></tr>");
+             echo "</table>";
+
+               
+
+
+            ?>
+            
+            </table>       
+            
+            <input type="submit" name="submit" value="submit"/>
+
+        </form>
+    </body>
+</HTML>    
