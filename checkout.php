4b6e4c7e796f882b8bbb83e4bf1a73d49d782324
lefriedberg <lefriedberg@mail.ru>
2015-11-01 21:11:49 -0500

    validation

--- checkout.php
+++ checkout.php
@@ -8,35 +8,44 @@
         $id = $_POST['id'];
         $name = $_POST['name'];
         
+        $error = "";
         
-        //todo check if name/custID/address/area are valid
+        if(preg_match("/^\d\d?$", $area) == 0){
+            $error.="You forgot to enter a valid area. ";
+        }
+        if(empty($id) && empty($name)){
+            $error.= "Please enter a valid customer ID or your name.";
+        }
         
         
+        if($error == ""){
+            $db = mysql_connect("localhost","root","");
+            if(!$db) die("Error connecting to MySQL database.");
+            mysql_select_db("delivery" ,$db);
+            $result = mysql_query("SELECT * FROM food");
+
+            $items = ""; // list of item ids
+            $price = 0; //total price of the order
+
+            while($row= mysql_fetch_array($result)){
+                if($_POST["".$row['id']] == 'true'){
+                    $items .= $row['id'] . " ";
+                    $price += $row['price'];
+                }
+            } 
+
+
+
+
+            mysql_query("INSERT INTO order ()"); //todo create the order
+
+
+
+            exit();
+        }else{
+            echo $error;
+        }
         
-        $db = mysql_connect("localhost","root","");
-        if(!$db) die("Error connecting to MySQL database.");
-        mysql_select_db("delivery" ,$db);
-        $result = mysql_query("SELECT * FROM food");
-
-        $items = ""; // list of item ids
-        $price = 0; //total price of the order
-
-        while($row= mysql_fetch_array($result)){
-            if($_POST["".$row['id']] == 'true'){
-                $items .= $row['id'] . " ";
-                $price += $row['price'];
-            }
-        } 
-
-
-
-
-        mysql_query("INSERT INTO order ()"); //todo create the order
-
-
-
-        exit();
-
         function PrepSQL($value) {
                 // Stripslashes
                 if(get_magic_quotes_gpc()) {
