918db2c1961546f679cfd99cb39dbf440547c114
lefriedberg <lefriedberg@mail.ru>
2015-11-01 21:38:14 -0500

    this should work?

--- checkout.php
+++ checkout.php
@@ -38,15 +38,18 @@
                 mysql_query("INSERT INTO customer(name, num_orders) VALUES (".PrepSQL($name).",1)");
                 $newid = mysql_query("SELECT ID FROM customer WHERE name = " . PrepSQL($name) . " ORDER BY id DESC LIMIT 1");
                 $row = mysql_fetch_array($newid);
+                $id = $row['ID'];
                 echo "Your new customer ID is ".$row['ID'];
             }else{
                 mysql_query("UPDATE customer SET num_rows = num_rows + 1 WHERE id = " . PrepSQL($id));
             }
 
+            $boys = mysql_query("SELECT * FROM boy WHERE area = ".PrepSQL($area));
+            $row = mysql_fetch_array($boys);
+            echo "Your boy is ". $row['name'];
+            //assigns you a boy
 
-            mysql_query("INSERT INTO order ()"); //todo create the order
-
-
+            mysql_query("INSERT INTO order (description, price, boy_id, customer_id) VALUES (".PrepSQL($items).", ".$price.", ".$row[id].", ".PrepSQL($id)); //creates the order
 
             exit();
         }else{
